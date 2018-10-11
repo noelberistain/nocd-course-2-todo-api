69e4439f84201b86bdf1e8a0347d4605d49126e2
noel <noelberistain@gmail.com>
2018-10-11 14:22:37 -0700

    gave up

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,7 @@ mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://127.0.0.1:27017/TodoApp',{ useNewUrlParser: true });
 
 //FOR MLAB MONGODB AT HEROKU we should use the URI value to connect
-
-mongoose.connect('mongodb://test1:b3r15t41n@ds129823.mlab.com:29823/todo-api',{ useNewUrlParser: true });// || 'mongodb://localhost:27017/TodoApp'
+mongoose.connect('mongodb://test1:b3r15t41n@ds129823.mlab.com:29823/todo-api'||'mongodb://127.0.0.1:27017/TodoApp',{ useNewUrlParser: true }); 
 
 // module.exports = {
 //     mongoose: mongoose
